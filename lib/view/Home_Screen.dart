35811a4f9ef6e284557221fd2b60b5db5a5479e6
Muhammad Nasir <122076646+Nasir747@users.noreply.github.com>
2023-01-26 09:19:05 -0800

    new commit

--- lib/view/Home_Screen.dart
+++ lib/view/Home_Screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../CUSTOM_WIDGETS/transaction_container.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -11,33 +13,221 @@ class Home extends StatelessWidget {
       body: Container(
         height: double.infinity,
         width: double.infinity,
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * .2,
-              color: Colors.transparent.withOpacity(0),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.amber,
-                    backgroundImage:
-                        AssetImage('assets/icons/Genny_icon_transpart.png'),
-                  ),
-                  ///////////////////////////////////////////////////////
-                  CircleAvatar(
-                    backgroundColor: Colors.white24,
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.search,
-                          color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * .1,
+                color: Colors.transparent.withOpacity(0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                        backgroundColor: Colors.amber,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image(
+                              image: const AssetImage(
+                                  'assets/icons/BhaiNabeel.jpg')),
                         )),
-                  )
-                ],
+                    ///////////////////////////////////////////////////////
+                    CircleAvatar(
+                      backgroundColor: Color.fromARGB(60, 79, 64, 64),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.notifications, color: Colors.black)),
+                    ),
+                  ],
+                ),
+                //////////////////////////////////////////////////////////
               ),
-            )
-          ],
+              //////////////////////////////////////////////////////////////////////////
+              ///////////////////////////////////////////////////////////////////////
+              Text(
+                'Hello',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 57, 52, 52),
+                    fontSize: MediaQuery.of(context).size.height * .02,
+                    fontWeight: FontWeight.bold),
+              ),
+              //////////////////////////////////////////////////////////////
+              ///////////////////////////////////////////////////////////////////////
+              Text(
+                'Nabeel Ahmad',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: MediaQuery.of(context).size.height * .04,
+                    fontWeight: FontWeight.bold),
+              ),
+              //////////////////////////////////////////////////////////////
+              Container(
+                margin: EdgeInsets.only(top: 25),
+                height: MediaQuery.of(context).size.height * .3,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(10, 35),
+                          blurRadius: 200,
+                          color: Colors.blue)
+                    ]),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 30, left: 5, right: 0, bottom: 15),
+                        child: Text(
+                          'Your Balance ...',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize:
+                                  MediaQuery.of(context).size.height * .02,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                      //////////////////////////////////////////////////////////////
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 60, left: 5, right: 0, bottom: 20),
+                        child: Text(
+                          '987654321 pkr',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize:
+                                  MediaQuery.of(context).size.height * .03,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                      //////////////////////////////////////////////////////////////
+                    ],
+                  ),
+                ),
+              ),
+              ////////////////////////////////////////////////////////////////////////////
+              ////////////////////////////////////////////////////////////////
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 20, bottom: 8, left: 8, right: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'LAST DEBIT',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 96, 85, 85),
+                              fontSize:
+                                  MediaQuery.of(context).size.height * .015,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        //////////////////////////////////////////////////
+                        Text(
+                          'RS/- 987654321',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize:
+                                  MediaQuery.of(context).size.height * .02,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        /////////////////////////////////////////
+                      ],
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.red,
+                      child: Icon(
+                        Icons.arrow_circle_up,
+                        color: Colors.white,
+                      ),
+                    ),
+                    /////////////////////////////////////////////////////////
+
+                    Container(
+                      color: Colors.blue,
+                      height: MediaQuery.of(context).size.height * .04,
+                      width: 1,
+                    ),
+                    /////////////////////////////////////////////
+                    ///
+                    CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      child: Icon(
+                        Icons.arrow_circle_up,
+                        color: Colors.white,
+                      ),
+                    ),
+                    ////////////////////////////////////////
+                    Column(
+                      children: [
+                        Text(
+                          'LAST CREDIT',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 96, 85, 85),
+                              fontSize:
+                                  MediaQuery.of(context).size.height * .015,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        //////////////////////////////////////////////////
+                        Text(
+                          'RS/- 123456789',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize:
+                                  MediaQuery.of(context).size.height * .02,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        /////////////////////////////////////////
+                      ],
+                    ),
+
+                    /////////////////////////////////////////////////////////
+                  ],
+                ),
+              ),
+              //////////////////////////////////////////////////////////////////////////////////
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 15),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * .05,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(.5),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(20, -1),
+                          blurRadius: 20,
+                          color: Colors.blue,
+                        )
+                      ]),
+                  child: Center(
+                      child: Text(
+                    'New Offers : ',
+                    style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height * .04),
+                  )),
+                ),
+              ),
+              //////////////////////////////////////////////////////////////////////////////////
+              /*
+              ListView.builder(
+                itemCount: 3,
+                itemBuilder: (BuildContext context, int index) {
+                  return transaction_container();
+                },
+              ),
+              */
+              //////////////////////////////////////////////
+              transaction_container(),
+              transaction_container(),
+              transaction_container(),
+            ],
+          ),
         ),
       ),
     );
