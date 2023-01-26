7368dc0729bbb442dd41c5e40a29c6cc27401793
Asad <asads8864@gmail.com>
2023-01-26 13:40:40 +0500

    abi baja ha

--- lib/view/Login_Screen.dart
+++ lib/view/Login_Screen.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              TextFormField(decoration: InputDecoration(label: Text("Username"))),
+              TextFormField(decoration: InputDecoration(label: Text("Password"))),
+              ElevatedButton(onPressed: () {}, child: Text("Sign")),
+              Row(
+                children: [Text("Don't have an account?"),
+
+                TextButton(onPressed: (){}, child: Text("Sign up")),
+
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
